928a418bc7d387e5f43a449a6364d0da3ecd0bf6
ivzhang1 <izhang1@stuy.edu>
2017-11-20 12:59:52 -0500

    Added 01SuperArray and 02OrderedSuperArray

---
+++ 01SuperArray/SuperArray.java
@@ -0,0 +1,166 @@
+public class SuperArray{
+    private String[] data;
+    private int size;
+
+    public SuperArray(){
+      data = new String[10];
+      size = 0;
+    }
+  
+    public String[] getData(){
+      return data;
+    }
+
+    public int size(){
+      return size;
+    }
+
+    public void clear(){
+      data = new String[size];
+      size = 0;
+    }
+
+    public boolean add(String element){
+      
+      if (size == data.length){
+        this.resize();
+      }
+      
+      data[size] = element;
+      size += 1;
+      return true;
+    }
+
+    private void resize(){
+      String[] newArray;
+      if (size == 0){
+        newArray = new String[size + 1];
+      }
+      else{
+        newArray = new String[size * 2];
+      }
+      for (int i = 0; i < size; i++){
+        newArray[i] = data[i];
+      }
+      data = newArray;
+    }
+
+    private void resize(int times){
+      for (int i = 0; i < times; i++){
+        resize();
+      }
+    }
+  
+    public boolean isEmpty(){
+      return size() == 0;
+    }
+  
+    public String get(int index){
+      if (index >= size() || index < 0){
+        System.out.println("INDEX OUT OF RANGE");
+        return null;
+      }
+      else{
+        return data[index];
+      }
+    }
+
+    public String set(int index, String element){
+      if (index >= size() || index < 0){
+        System.out.println("INDEX OUT OF RANGE");
+        return null;
+      }
+      
+      else{
+        String oldElement = data[index];
+        data[index] = element;
+        return oldElement;
+      }
+    }
+
+    public boolean contains(String element){
+      for (int i = 0; i < size; i++){
+        if (data[i].equals(element)){
+            return true;
+        }
+      }
+      return false;
+    }
+
+    public void add(int index, String element){
+      if (index > size || index < 0){
+        System.out.println("INDEX OUT OF RANGE");
+      }
+      else{
+        if (size == data.length){
+          this.resize();
+        }
+        for (int i = size - 1; i >= index; i -= 1){
+          data[i + 1] = data[i];
+        }
+        data[index] = element;
+        size ++;
+      }
+    }
+
+    public int indexOf(String element){
+
+      for (int i = 0 ; i < size; i ++){
+        if (data[i].equals(element)){
+          return i;
+        }
+      }
+      return -1;
+      
+    }
+
+    public int lastIndexOf(String element){
+      for (int i = size - 1; i > 0; i -= 1){
+        if (data[i].equals(element)){
+          return i;
+        }
+      }
+      return -1;
+    }
+
+    public String remove(int index){
+      if (index >= size || index < 0){
+        System.out.println("INDEX OUT OF RANGE");
+        return null;
+      }
+      else{
+        String removed = data[index];
+          
+        for (int i = index; i < size; i++){
+            data[i] = data[i+1];
+        }
+        size -= 1;
+        
+        return removed;
+      }
+    }
+
+    public boolean remove(String element){
+      if (this.contains(element)){
+            int found = this.indexOf(element);
+            this.remove(found);
+            return true;
+      }
+      else{
+            return false;
+      }
+    }
+
+    public String toString(){
+      String finished = "[";
+      if (size == 0){
+          return finished + "]";
+      }
+      for (int i = 0; i < size - 1; i++){
+        finished += data[i] + ", ";
+      }
+
+      return finished += data[size - 1] + "]";
+    }
+  
+}
