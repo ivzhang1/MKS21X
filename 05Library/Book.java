7531877fdd5f8dbb6b8c8cd7445a100bb4d9399a
ivzhang1 <izhang1@stuy.edu>
2017-11-28 13:22:50 -0500

    Added Book.java

---
+++ 05Library/Book.java
@@ -0,0 +1,38 @@
+public class Book{
+
+    //Field Variables for Book
+    public String author, title, ISBN;
+
+    //Constructors for Book
+    public Book(){
+
+    }
+    public Book(String author, String title, String ISBN){
+	this.author = author;
+	this.title = title;
+	this.ISBN = ISBN;
+    }
+
+    //Getters for Book
+    public String getAuthor(){
+	return author;
+    }
+    public String getTitle(){
+	return title;
+    }
+    public String getISBN(){
+	return ISBN;
+    }
+
+    //Setters for Book
+    public void setAuthor(String author){
+	this.author = author;
+    }
+    public void setTitle(String title){
+	this.title = title;
+    }
+    public void setISBN(String ISBN){
+	this.ISBN = ISBN;
+    }
+    
+}
