f2413ecf8dd78f4bfc8e7ccf45e6bf20c3339022
Ivan Zhang <ivantheterrible@Ivans-MacBook-Pro.local>
2017-12-09 22:43:50 -0500

    Added bubbleSort() and more extensive tests

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -3,21 +3,49 @@ import java.util.Arrays;
 public class Sorts{
 
   public static void main(String[]artie){
+    long startTime = System.currentTimeMillis();
     int[] randish = new int[1000];
-    for(int i = 0 ; i < randish.length; i++){
-      randish[i] =(int)(Math.random()*100);
+    //int[] randish = new int[0];
+    //int[] randish = new int[1];
+    //int[] randish = new int[2];
+    //int[] randish = new int[3];
+    //int[] randish = new int[100000];
+
+    for(int i = 0 ; i < randish.length; i++){ //PURELY RANDOM
+      randish[i] = (int)(Math.random()*100);
     }
+
+    //for(int i = 0 ; i < randish.length; i++){ //ALL THE SAME NUMBER OR YOU CAN JUST RUN MAIN WITHOUT ANY FOR LOOPS
+    //  randish[i] = 1;
+    //}
+
+    //for(int i = 0 ; i < randish.length; i++){ //BIGGEST TO SMALLEST
+    //  randish[i] = randish.length - i;
+    //}
+
+    //for(int i = 0 ; i < randish.length; i++){ //SMALLEST TO BIGGEST
+    //  randish[i] = i;
+    //}
 
     System.out.println(Arrays.toString(randish));
 
     //Sorts Tests Here:
-    //System.out.println("selectionSort: \n");
+
+    //System.out.println("selectionSort:");
     //selectionSort(randish);
 
-    System.out.println("insertionSort: \n");
-    insertionSort(randish);
+    //System.out.println("insertionSort:");
+    //insertionSort(randish);
+
+    System.out.println("bubbleSort:");
+    bubbleSort(randish);
 
     System.out.println(Arrays.toString(randish));
+
+    System.out.println("Runtime: " +
+                       (System.currentTimeMillis() - startTime) + "ms or " +
+                       (System.currentTimeMillis() - startTime)/1000.0 + "s"
+                       );
 
   }
 
@@ -62,12 +90,30 @@ public class Sorts{
       }
       temp = data[n];
       for (int k = n; k < i; k++){
-	temp2 = data[k+1];
+        temp2 = data[k+1];
         data[k+1] = temp;
         temp = temp2;
       }
       data[n] = place;
 
+    }
+
+  }
+
+  public static void bubbleSort(int[] data){
+    int upTo = data.length - 1;
+    int temp = 0;
+
+    while (upTo > 0){
+      for (int i = 0; i < upTo; i++){
+        if (data[i] > data[i + 1]){
+          temp = data[i+1];
+          data[i+1] = data[i];
+          data[i] = temp;
+        }
+      }
+
+      upTo -= 1;
     }
 
   }
